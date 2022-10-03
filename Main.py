8af38165a09a2fb6b592cd5afb8a011cc94df518
Pratheesh URK21CS1064 <109951975+URK21CS1064@users.noreply.github.com>
2022-10-03 14:44:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,23 +1,18 @@
 from typing import List
-
-def merge_sort(data) -> None:
-  # Write code here
-   size=len(data)
-   for index in range(size):
-    small_index = index
-    for jindex in range(index+1, size):
-      if array[jindex] < array[small_index]:
-        small_index = jindex
-    array[small_index],array[index] = array[index], array[small_index]
-    return array
-
-# Do not change the following code
+def merge_sort(array):
+    size=len(data)
+    for index in range(size):
+        small_index = index
+        for jindex in range(index+1, size):
+            if array[jindex] < array[small_index]:
+                small_index = jindex
+        array[small_index],array[index] = array[index], array[small_index]
+    print(array)
 input_data = input()
 data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
- merge_sort(data)
-print(data)
+for item in input_data.split(','):
+    if item.isnumeric():
+        data.append(int(item))
+    elif item.lstrip("-").isnumeric():
+        data.append(int(item))
+merge_sort(data)
